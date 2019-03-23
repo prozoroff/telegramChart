cdabd3685f308facb3559f7a77a368b954bbd419
Alex Prozorov <alexprozoroff@gmail.com>
2019-03-23 10:14:57 +0300

    animation types

--- src/animation.js
+++ src/animation.js
@@ -2,33 +2,79 @@ const reqAnimFrame = window.requestAnimationFrame ||
                window.mozRequestAnimationFrame ||
                window.webkitRequestAnimationFrame
 
-const startFraction = 1 / 10
-const deltaFraction = startFraction / 20
+const linear = (stepsCount, from, to) => ({
+    step: 0,
+    get val () {
+        if (this.step === stepsCount) {
+            return null
+        }
+        let time = this.step++ / stepsCount
+        return (to - from) * time + from
+    }
+})
+
+const easeOut = (stepsCount, from, to) => ({
+    step: 0,
+    get val () {
+        if (this.step === stepsCount) {
+            return null
+        }
+        let time = this.step++ / stepsCount - 1
+        return (to - from) * (time * time * time + 1) + from
+    }
+})
+
+const easeIn = (stepsCount, from, to) => ({
+    step: 0,
+    get val () {
+        if (this.step === stepsCount) {
+            return null
+        }
+        let time = this.step++ / stepsCount
+        return (to - from) * time * time * time + from
+    }
+})
+
+const easeInOut = (stepsCount, from, to) => ({
+    step: 0,
+    get val () {
+        if (this.step === stepsCount) {
+            return null
+        }
+        let time = 2 * this.step++ / stepsCount
+
+        if (time < 1) {
+            return (to - from) / 2 * time * time + from
+        }
+        time -= 1
+        return (from - to) / 2 * (time * (time - 2) - 1) + from
+    }
+})
 
 export const animate = (series, toY, toScale) => {
-    let fraction = startFraction
     const fromY = series.translate[1]
     const fromScale = series.scale[1]
 
-    const done = () => toY - fromY > 0
-        ? series.translate[1] >= toY
-        : series.translate[1] <= toY
+    const stepsCount = 20
+    const yAnim = easeOut(stepsCount, fromY, toY)
+    const scaleAnim = easeOut(stepsCount, fromScale, toScale)
 
     let stop
     const cancel = () => (stop = true)
 
     const render = () => {
-        if (!stop) {
-            reqAnimFrame(render)
-        }
-        series.translate[1] += fraction * (toY - fromY)
-        series.scale[1] += fraction * (toScale - fromScale)
-        fraction -= deltaFraction
+        const yVal = yAnim.val
+        const scaleVal = scaleAnim.val
 
-        if (done()) {
+        if (yVal !== null && !stop) {
+            reqAnimFrame(render)
+            series.translate[1] = yVal
+            series.scale[1] = scaleVal
+        }
+
+        if (yVal === null) {
             series.translate[1] = toY
             series.scale[1] = toScale
-            stop = true
         }
 
         series.setTransform()
